9f40749bf221107698b4939acac0d880310a57c8
mongarerobert3 <mongarerobert3@gmail.com>
2024-07-10 17:47:03 +0300

    hosts

--- dairy_management/dairy_management/settings.py
+++ dairy_management/dairy_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://farm-data.onrender.com']
+ALLOWED_HOSTS = ['https://farm-data.onrender.com']
 
 
 # Application definition
